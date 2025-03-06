fb81e1842b67f08b17eff850d91f3511adb0245d
Kaylie Craig <wowitskaylie@gmail.com>
2025-03-06 10:53:30 -0500

    Initial commit

---
+++ BananaOS-XSRedirector/Patches/BananaPatch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HarmonyLib;
+
+namespace BananaOS_XSRedirector.Patches
+{
+    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch("Awake", MethodType.Normal)]
+    internal class BananaPatch
+    {
+        private static void Postfix(GorillaLocomotion.Player __instance)
+        {
+            Console.WriteLine(__instance.maxJumpSpeed);
+        }
+    }
+}
