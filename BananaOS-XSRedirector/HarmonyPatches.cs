b9d2dc428ee36f19e0060a121c799498a34215d1
Kaylie Craig <wowitskaylie@gmail.com>
2025-03-06 14:49:53 -0500

    rider what the fuck

--- BananaOS-XSRedirector/HarmonyPatches.cs
+++ BananaOS-XSRedirector/HarmonyPatches.cs
@@ -4,38 +4,38 @@ using HarmonyLib;
 
 namespace BananaOS_XSRedirector
 {
-	/// <summary>
-	/// This class handles applying harmony patches to the game.
-	/// You should not need to modify this class.
-	/// </summary>
-	public class HarmonyPatches
-	{
-		private static Harmony instance;
+    /// <summary>
+    /// This class handles applying harmony patches to the game.
+    /// You should not need to modify this class.
+    /// </summary>
+    public class HarmonyPatches
+    {
+        private static Harmony instance;
 
-		public static bool IsPatched { get; private set; }
-		public const string InstanceId = PluginInfo.GUID;
+        public static bool IsPatched { get; private set; }
+        public const string InstanceId = PluginInfo.GUID;
 
-		internal static void ApplyHarmonyPatches()
-		{
-			if (!IsPatched)
-			{
-				if (instance == null)
-				{
-					instance = new Harmony(InstanceId);
-				}
+        internal static void ApplyHarmonyPatches()
+        {
+            if (!IsPatched)
+            {
+                if (instance == null)
+                {
+                    instance = new Harmony(InstanceId);
+                }
 
-				instance.PatchAll(Assembly.GetExecutingAssembly());
-				IsPatched = true;
-			}
-		}
+                instance.PatchAll(Assembly.GetExecutingAssembly());
+                IsPatched = true;
+            }
+        }
 
-		internal static void RemoveHarmonyPatches()
-		{
-			if (instance != null && IsPatched)
-			{
-				instance.UnpatchSelf();
-				IsPatched = false;
-			}
-		}
-	}
+        internal static void RemoveHarmonyPatches()
+        {
+            if (instance != null && IsPatched)
+            {
+                instance.UnpatchSelf();
+                IsPatched = false;
+            }
+        }
+    }
 }
--- BananaOS-XSRedirector/PluginInfo.cs
+++ BananaOS-XSRedirector/PluginInfo.cs
@@ -1,12 +1,12 @@
 ﻿namespace BananaOS_XSRedirector
 {
-	/// <summary>
-	/// This class is used to provide information about your mod to BepInEx.
-	/// </summary>
-	class PluginInfo
-	{
-		public const string GUID = "com.luna.gorillatag.bananaos-xsredirector";
-		public const string Name = "BananaOS_XSRedirector";
-		public const string Version = "1.0.0";
-	}
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.luna.gorillatag.bananaos-xsredirector";
+        public const string Name = "BananaOS_XSRedirector";
+        public const string Version = "1.0.0";
+    }
 }
